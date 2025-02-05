c7d0a1aa1b29bb44c8aada8601aa88dae5855cc6
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-05 16:30:35 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Background from './components/background'
+export default function App() {
+  return (
+   <>
+   <Background/>
+   
+   </>
+  )
+}
---
+++ src/components/background.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function background() {
+  return (
+    <div className='fixed w-[480px] h-screen -z-10 '>
+        <img className='fit' src="campo_de_flores_con_variaciones_de_colores_muy_coloridos_en_un_estilo_de_van_gogh__ttymf2zyrtijv5i0ujsh_3.png" alt="" /></div>
+  )
+}
